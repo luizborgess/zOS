f18fde74623c94f7b09b9f944af46cf209afa4fd
Luiz <luiz.borgess@hotmail.com>
2026-02-04 14:57:00 -0300

    add docker

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,6 +9,19 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
+
+## Add docker
+
+dnf config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
+sed -i "s/enabled=.*/enabled=0/g" /etc/yum.repos.d/docker-ce.repo
+dnf -y install --enablerepo=docker-ce-stable \
+    containerd.io \
+    docker-buildx-plugin \
+    docker-ce \
+    docker-ce-cli \
+    docker-compose-plugin \
+    docker-model-plugin
+
 # this installs a package from fedora repos
 #dnf5 install -y htop
 #dnf5 install -y @gnome-desktop
